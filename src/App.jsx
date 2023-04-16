17b1e2e6ddca6a3db6d242a07512aab0a24c09a4
midCenturyDude7 <mgriffes@gmail.com>
2023-04-16 10:32:17 -0700

    Working on Details component and adding React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SearchParams from "./SearchParams";
+import Details from "../Details";
 
 const App = () => {
   return (
-    <div>
+    <Router>
       <h1>Adopt Me!</h1>
+      <Routes>
+        <Route path="/details/:id" element={<Details />} />
+        <Route path="/" element={<SearchParams />} />
+      </Routes>
       <SearchParams />
-    </div>
+    </Router>
   );
 };
 
